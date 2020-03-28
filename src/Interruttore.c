e364231591487dbbc6ba048f9402c94e4285ca90
TobiaP <tpavona1@gmail.com>
2020-03-28 16:46:52 +0100

    Add files via upload

---
+++ src/Interruttore.c
@@ -0,0 +1,5 @@
+/*
+	si possono impostare come interruttori o pulsanti in
+	base a un parametro in fase di esecuzione e si possono
+	poi manovrare. Comunicano solo con il corrispondente S(i)
+*\
---
+++ src/Led.c
@@ -0,0 +1,4 @@
+/*
+	I led hanno uno stato ON/OFF verificabile, comunicano solo con il
+	corrispondente S(i) 
+*/
