07b472e7eb94fde5068e110d327f7d4011dc2b0e
TobiaP <tpavona1@gmail.com>
2020-04-20 11:34:04 +0200

    Add files via upload

---
+++ util.h
@@ -0,0 +1,38 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+struct mesg_buffer { 
+    long mesg_type; 
+    char mesg_text[100]; 
+} message; 
+
+#define path_pipe "/tmp/ipc/"
+#define path_L "bin/Led.c"
+#define path_B "bin/Button.c"
+#define path_S "bin/Switc.c"
+#define arg_0 "1"
+#define MAX_BUFF_SIZE 1024
+
+#endif
