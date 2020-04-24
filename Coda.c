337ae92cf2d713d07d5c5f33d00f1cc2156c1b0d
TobiaP <tpavona1@gmail.com>
2020-04-24 15:49:22 +0200

    Update Coda.c

--- Coda.c
+++ Coda.c
@@ -1,43 +1,5 @@
 
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <float.h>
-#include <limits.h>
-#include <math.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h>
-#include <unistd.h>
-
-#define MAX_QUEUE 100
-#define path_pipe "/tmp/ipc/"
-#define path_L "bin/Led.c"
-#define path_B "bin/Button.c"
-#define path_S "bin/Switc.c"
-#define path_P "bin/Processo.c"
-#define arg_0 "1"
-#define MAX_BUFF_SIZE 1024
-
-struct mesg_buffer { 
-    long mesg_type; 
-    char mesg_text[100]; 
-} message; 
-
-
-/*^^^^^^^^^^^^^^*/
-/*util.h*/
+#include "util.h"
 
 int N_proc = 1;                     /*numero di processi attuale >= 1*/
 unsigned long long int nuovo_id=0;  /*ultima identità creata*/
