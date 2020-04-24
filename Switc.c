0423256eef9d7349efb71a76977af23e90c9392a
GaiaFaggin <62758569+GaiaFaggin@users.noreply.github.com>
2020-04-24 15:50:36 +0200

    Update Switc.c

--- Switc.c
+++ Switc.c
@@ -1,32 +1,10 @@
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <float.h>
-#include <limits.h>
-#include <math.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h>
-#include <unistd.h>
-
 struct mesg_buffer { 
     long mesg_type; 
     char mesg_text[100]; 
 } message; 
 
-/*^^^^^^^^^^^^^^
-util.h*/
+
+#include "util.h"
 
 int pid;
 
