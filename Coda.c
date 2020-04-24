6b4ce52d6e6053a8eb8cc74ea0f78c336ad34e4b
TobiaP <tpavona1@gmail.com>
2020-04-24 15:41:50 +0200

    Update Coda.c

--- Coda.c
+++ Coda.c
@@ -36,21 +36,21 @@ struct mesg_buffer {
 } message; 
 
 
-//^^^^^^^^^^^^^^
-//util.h
+/*^^^^^^^^^^^^^^*/
+/*util.h*/
 
-int N_proc = 1;                     //numero di processi attuale >= 1
-unsigned long long int nuovo_id=0;  //ultima identità creata
-unsigned long long int size_Coda=0; //numero messaggi in queue
-int pipeF[2][2];                    //vengono create due pipe con cui comunicare ai figli in modo che si possa cancellare 
-                                    //un figlio e nel frattempo creare l'altro
-unsigned long long int selezionato=0;   //processo selezionato da C (default 0), se si preme un altro T torna a 0
+int N_proc = 1;                     /*numero di processi attuale >= 1*/
+unsigned long long int nuovo_id=0;  /*ultima identitÃ  creata*/
+unsigned long long int size_Coda=0; /*numero messaggi in queue*/
+int pipeF[2][2];                    /*vengono create due pipe con cui comunicare ai figli in modo che si possa cancellare */
+                                    /*un figlio e nel frattempo creare l'altro*/
+unsigned long long int selezionato=0;   /*processo selezionato da C (default 0), se si preme un altro T torna a 0*/
 int pipeatt=0;
 pid_t primo_pid;
-int msgid, msgid_main;				//nome pipe con comandi, nome pipe con main
-unsigned long long int coda[MAX_QUEUE]; //tiene memorizzati tutti gli identificativi dei processi
+int msgid, msgid_main;				/*nome pipe con comandi, nome pipe con main*/
+unsigned long long int coda[MAX_QUEUE]; /*tiene memorizzati tutti gli identificativi dei processi*/
 
-unsigned long long int new_identity() //crea una nuova identità (nuovo_id+1)
+unsigned long long int new_identity() /*crea una nuova identitÃ  (nuovo_id+1)*/
 {
   nuovo_id++;
   return nuovo_id;
@@ -62,20 +62,20 @@ void crea_primo_figlio(unsigned long long int fident)
   pipe(pipeF[pipeatt]);
   pid_t pid_n;
   pid_n=fork();
-  if(pid_n==0)//creazione processo figlio
+  if(pid_n==0)/*creazione processo figlio*/
   {
-    close(pipeF[pipeatt][1]);    //il figlio è il lettore della pipe
-    dup2(pipeF[pipeatt][0], 0);  //ora il figlio legge dalla pipe con scanf()
-    close(pipeF[pipeatt][0]);    //si chiude la lettura originale
+    close(pipeF[pipeatt][1]);    /*il figlio Ã¨ il lettore della pipe*/
+    dup2(pipeF[pipeatt][0], 0);  /*ora il figlio legge dalla pipe con scanf()*/
+    close(pipeF[pipeatt][0]);    /*si chiude la lettura originale*/
     
     char* fid;
     sprintf(fid, "%lli", fident);
     execlp(path_P, fid);
     exit(0);
   }else{
-    close(pipeF[pipeatt][0]);    //questo processo deve inviare sulla pipe
-    dup2(pipeF[pipeatt][1], 1);  //questo processo permette di inviare con printf()
-    close(pipeF[pipeatt][1]);    //si chiude la scrittura originale
+    close(pipeF[pipeatt][0]);    /*questo processo deve inviare sulla pipe*/
+    dup2(pipeF[pipeatt][1], 1);  /*questo processo permette di inviare con printf()*/
+    close(pipeF[pipeatt][1]);    /*si chiude la scrittura originale*/
     primo_pid = pid_n;
   }
   size_Coda=1;
@@ -89,13 +89,13 @@ void invia_messaggio(char* mess)
   msgsnd(msgid_main, &message, sizeof(message), 1);
 }
 
-void inserisci_figlio(unsigned long long int nident) //inserisce un figlio in fondo alla coda
+void inserisci_figlio(unsigned long long int nident) /*inserisce un figlio in fondo alla coda*/
 {
   
-  printf("B");                          //scrivo al figlio che voglio aggiungere un Processo
+  printf("B");                          /*scrivo al figlio che voglio aggiungere un Processo*/
   char* nid;                            
   sprintf(nid, "%lli", nident); 
-  printf("%s", nid);                    //scrivo al figlio l'identità del nuovo processo
+  printf("%s", nid);                    /*scrivo al figlio l'identitÃ  del nuovo processo*/
       
 }
 
@@ -106,9 +106,9 @@ int main(int argc, char* argv[])
   crea_primo_figlio(new_identity());
   
   key_t	key_m = ftok("/tmp/ipc/mqueues", getppid());
-  msgid_main = msgget(key_m, 0666|IPC_CREAT); 		//si apre la pipe con il main
+  msgid_main = msgget(key_m, 0666|IPC_CREAT); 		/*si apre la pipe con il main*/
 
-  key_t key = ftok("/tmp/ipc/mqueues", getpid());   //si apre la pipe per la comunicazione con i comandi
+  key_t key = ftok("/tmp/ipc/mqueues", getpid());   /*si apre la pipe per la comunicazione con i comandi*/
   msgid = msgget(key, 0666|IPC_CREAT);
 
 
@@ -119,13 +119,13 @@ int main(int argc, char* argv[])
     {
       switch(message.mesg_text[0])
       {
-        case 'A':   //rimozione del primo figlio
+        case 'A':   /*rimozione del primo figlio*/
         {
           selezionato=0;
           if(size_Coda>1)
           {
             size_Coda--; 
-            for(int i=0; i<MAX_QUEUE; i++)  //si aggiorna la coda eliminando il primo identificativo
+            for(int i=0; i<MAX_QUEUE; i++)  /*si aggiorna la coda eliminando il primo identificativo*/
             {
               if(i>=size_Coda)
                 coda[i]=0;
@@ -138,16 +138,16 @@ int main(int argc, char* argv[])
             else pipe(pipeF[1]);
             pid_t pid_n;
             pid_n=fork();
-            if(pid_n==0)//creazione processo figlio (ora in modo che non rimanga un P attivo)
+            if(pid_n==0)/*creazione processo figlio (ora in modo che non rimanga un P attivo)*/
             {
-              if(pipeatt==1)  //si utilizza la pipe che non è in uso dal figlio in eliminazione
+              if(pipeatt==1)  /*si utilizza la pipe che non Ã¨ in uso dal figlio in eliminazione*/
               {
                 close(pipeF[0][1]);
-                dup2(pipeF[0][0], 0);  //ora il figlio legge dalla pipe con scanf()
+                dup2(pipeF[0][0], 0);  /*ora il figlio legge dalla pipe con scanf()*/
                 close(pipeF[0][1]);
               }else{
                 close(pipeF[1][1]);
-                dup2(pipeF[1][0], 0);  //ora il figlio legge dalla pipe con scanf()
+                dup2(pipeF[1][0], 0);  /*ora il figlio legge dalla pipe con scanf()*/
                 close(pipeF[1][1]);
               }
               char* fid;
@@ -156,16 +156,16 @@ int main(int argc, char* argv[])
               exit(0);
             }
 
-            printf("%c", 'E');    //si comunica al primo processo che tutti vanno eliminati
+            printf("%c", 'E');    /*si comunica al primo processo che tutti vanno eliminati*/
             size_Coda=1;
 
-            if(pipeatt==0)        //si cambia la pipe anna attuale
+            if(pipeatt==0)        /*si cambia la pipe anna attuale*/
               pipeatt=1;
             else pipeatt=0;
 
-            close(pipeF[pipeatt][0]);    //questo processo deve inviare sulla pipe
-            dup2(pipeF[pipeatt][1], 1);  //questo processo permette di inviare con printf()
-            close(pipeF[pipeatt][1]);    //si chiude la scrittura originale
+            close(pipeF[pipeatt][0]);    /*questo processo deve inviare sulla pipe*/
+            dup2(pipeF[pipeatt][1], 1);  /*questo processo permette di inviare con printf()*/
+            close(pipeF[pipeatt][1]);    /*si chiude la scrittura originale*/
               
             primo_pid = pid_n;
 
@@ -174,40 +174,40 @@ int main(int argc, char* argv[])
               inserisci_figlio(coda[i]);
             }
             
-            invia_messaggio("Il nodo è stato eliminato con successo\n");
+            invia_messaggio("Il nodo Ã¨ stato eliminato con successo\n");
           }else
             invia_messaggio("non ci sono abbastanza elementi nella coda\n");
         }break;
 
-        case 'B':   //aggiunta di un nuovo figlio in fondo
+        case 'B':   /*aggiunta di un nuovo figlio in fondo*/
         {
           selezionato=0;
           if(size_Coda<MAX_QUEUE)
           {
             inserisci_figlio(new_identity());
-            size_Coda++;                          //la coda ha un elemento in più 
-            coda[size_Coda]=nuovo_id;             //aggiungo l'ultimo elemento in coda
-            invia_messaggio("il nodo è stato aggiunto con successo\n"); //viene inviato al main un messaggio di successo
+            size_Coda++;                         /*la coda ha un elemento in piÃ¹ */
+            coda[size_Coda]=nuovo_id;             /*aggiungo l'ultimo elemento in coda*/
+            invia_messaggio("il nodo Ã¨ stato aggiunto con successo\n"); /*viene inviato al main un messaggio di successo*/
           }else
-            invia_messaggio("limite processi in Coda raggiunto\n"); //altrimenti viene inviato un messaggio di errore al main
+            invia_messaggio("limite processi in Coda raggiunto\n"); /*altrimenti viene inviato un messaggio di errore al main*/
         }break;
 
-        case 'C':     //selezionare un elemento e invia la lista
-        {             //con l'elemento selezionato 
-          selezionato++;                 //l'elemento selezionato aumenta di uno ciclicamente
+        case 'C':     /*selezionare un elemento e invia la lista*/
+        {             /*con l'elemento selezionato */
+          selezionato++;                 /*l'elemento selezionato aumenta di uno ciclicamente*/
           if(selezionato>=size_Coda)
             selezionato = 0;
 
           char* lung;                             
           sprintf(lung, "%lli", size_Coda);
-          invia_messaggio(lung);          //invio la lunghezza della coda
+          invia_messaggio(lung);          /*invio la lunghezza della coda*/
           for(int i=0; i<size_Coda; i++)
           {
             char* mess;
             if(i!=selezionato)
               sprintf(mess, "%lli ", coda[i]);        
             else sprintf(mess, "(%lli) ", coda[i]);
-            invia_messaggio(mess);         //invio gli elementi
+            invia_messaggio(mess);         /*invio gli elementi*/
           }
         }break;
 
@@ -217,11 +217,11 @@ int main(int argc, char* argv[])
           {
           char* sel;
           sprintf(sel, "%lli", selezionato);
-          printf("%c", 'E');      //comunico che dall'elemento selezionato in poi vanno eliminati tutti
+          printf("%c", 'E');      /*comunico che dall'elemento selezionato in poi vanno eliminati tutti*/
           printf("%s", sel);
 
           size_Coda=selezionato;  
-          for(int i=selezionato; i<MAX_QUEUE; i++)  //aggiorno la coda togliendo il 
+          for(int i=selezionato; i<MAX_QUEUE; i++)  /*aggiorno la coda togliendo il */
           {
             if(i>=size_Coda)
               coda[i]=0;
@@ -239,11 +239,11 @@ int main(int argc, char* argv[])
             inserisci_figlio(coda[i]);
           selezionato=0;
 
-          invia_messaggio("Il figlio è stato eliminato con successo");
+          invia_messaggio("Il figlio Ã¨ stato eliminato con successo");
           }else invia_messaggio("non ci sono abbastanza messaggi in coda");
         }break;
 
-        case 'E': //il processo termina tutti i figli e poi termina
+        case 'E': /*il processo termina tutti i figli e poi termina*/
         {
           msgctl(msgid, IPC_RMID, NULL);
           printf("%c", 'E');
